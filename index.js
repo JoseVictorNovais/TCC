c5a12387be3c94e4be00975c5ed4ccd5784f6686
JoseVictorNovais <40804826+JoseVictorNovais@users.noreply.github.com>
2019-10-24 19:43:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import AppContainerMenu from './src/frontend/routes/routes';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AppContainerMenu);
